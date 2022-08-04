81da5d1ff4c5fe2f0f55c223e4ae5478b2a151f6
LlamaDaisy <laralowndes@googlemail.com>
2022-08-04 18:40:21 +0100

    1.0

---
+++ script.js
@@ -0,0 +1,65 @@
+let tvShow = "Friends"
+
+let characters = 6;
+
+console.log(tvShow)
+console.log(characters)
+
+
+characters = 8;
+
+console.log(characters);
+
+let ratings = "2.5"
+console.log(ratings);
+
+const hasShowFinished = true
+console.log(hasShowFinished)
+
+var myName = "Lara"
+var comment = `My name is ${myName} and I'm Learning JavaScript`
+console.log(comment)
+
+//let isBoxBlue = true;
+//if(isBoxBlue == false) {
+  //  alert("Box is not blue!");
+//} else {
+  //  alert("Box is blue!!!")
+// }
+
+//let AverageAge = 90;
+
+//if(AverageAge < 18) {
+  //  alert("Wow, they are kids!");
+//} else if(AverageAge > 18 && AverageAge < 70) {
+  //  alert("Wow they are adults");
+//} else {
+  //  alert("Wow they are super olds");
+//}
+
+//var partyAnswer = prompt("Would you like to come to the party?");
+
+//if(partyAnswer == 'yes')
+
+//{
+//    alert("We will see you there");
+//}
+
+//else if(partyAnswer == 'no')
+
+//{
+ //   alert("Too bad!!!");
+//} else {
+ //   alert("I wasn't expecting that!")
+//}
+
+let Fruit = ['Apple', 'Orange', 'Banana'];
+console.log(Fruit)
+
+let countries = ['England', 'France', 'Italy']
+console.log (countries)
+
+countries.push ('USA')
+console.log(countries)
+countries.splice('3')
+console.log(countries)
